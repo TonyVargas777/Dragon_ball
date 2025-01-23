78f6c2109a3a98aa6219868f5c62c46e1f4c65ae
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2025-01-23 13:27:01 +0100

    dragon ball

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        <img src="./db.avif" alt="" />
+      </div>
+      <section>
+        <img src="./drac.avif" alt="drac" />
+        <img src="./buu.avif" alt="buu" />
+        <img src="./cell.avif" alt="cell" />
+        <img src="./follet.avif" alt="follet" />
+        <img src="./freezer.avif" alt="freezer" />
+        <img src="./gohan.avif" alt="gohan" />
+        <img src="./goku.avif" alt="goku" />
+        <img src="./trunks.avif" alt="trunks" />
+        <img src="./vegeta.avif" alt="vegeta" />
+        <img src="./todos.avif" alt="todos" />
+
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
